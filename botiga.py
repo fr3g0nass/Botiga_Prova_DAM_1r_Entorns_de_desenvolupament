0d043d4e718b090a4cdc7f334c1ef0c8d6ebe9cb
jroca2 <jroca2@institutjaumehuguet.cat>
2026-03-05 19:05:59 +0100

    Versio Final codi1.3

--- botiga.py
+++ botiga.py
@@ -9,6 +9,7 @@ ELECTRONICA = "ELECTRONICA"
 
 #FUNCIONS
 def calcular_total(preu, tipus, edat):
+    """ Logica per aplicar el descompte/recarrec segons el tipus de producte"""
     
     total = preu
     
@@ -23,9 +24,14 @@ def calcular_total(preu, tipus, edat):
     #PER EVITAR VALORS NEGATIUS    
     if total < 0:
         total = 0
-        
-    print("El total es:")
-    print(res)
 
-calcular(100, "ROBA", 15)
-calcular(200, "ELECTRONICA", 40)
+    return total
+
+def mostrar_total(preu,tipus, edat):
+    """ Mostrar per la pantalla el total final de el producte"""
+    total =calcular_total(preu,tipus,edat)
+    print(f"El total per un producte de tipus '{tipus}' amb preu {preu}€ i edat {edat} és: {total}€")
+
+
+mostrar_total(100, ROBA, 15)
+mostrar_total(150, ELECTRONICA, 18)
